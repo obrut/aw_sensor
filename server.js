e1ab12fa8290328a1da136bc2ed65a2934199aaf
obrut <gustaf@fiholm.net>
2021-11-09 08:10:57 +0000

    Initial

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT;
+const HOST = '0.0.0.0';
+const MAX = process.env.MAX || 100;
+const MIN = process.env.MIN || 0;
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  const number = Math.floor(
+    Math.random() * (MAX - MIN + 1)
+  );
+  res.contentType('application/json');
+  const response = { topic: process.env.ID, payload: number };
+  res.end(JSON.stringify(response));
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
