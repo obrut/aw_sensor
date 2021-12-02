27b0a700fe4d200e32930dd0b3b782c39e93423b
Gustaf Ridderstolpe <gustaf.ridderstolpe@enfogroup.com>
2021-12-02 15:38:08 +0100

    sdf

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const HOST = process.env.HOST || '0.0.0.0';
 
 // App
 const app = express();
-const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const random = (min, max) => Math.floor(Math.random() * (max - min)+min);
 app.get('/', (req, res) => {
   res.contentType('application/json');
   const value = random(MIN, MAX);
